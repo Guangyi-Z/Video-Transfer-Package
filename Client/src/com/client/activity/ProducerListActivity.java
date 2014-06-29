d5f381a9de7b1f5d53142b1d2ee6da17a6cf71d8
LeeJay0226 <caolijie0226@163.com>
2014-06-29 11:40:08 +0800

    client trivials

--- Client/src/com/client/activity/ProducerListActivity.java
+++ Client/src/com/client/activity/ProducerListActivity.java
@@ -1,5 +1,10 @@
 package com.client.activity;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,6 +13,7 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
@@ -25,6 +31,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.androidsocket.R;
+import com.client.model.PacketBean;
 import com.client.model.ProducerBeans;
 import com.client.model.ServerBeans;
 import com.client.view.CustomDialog;
@@ -33,7 +40,7 @@ import com.client.view.CustomDialog;
  * 
  * @author caolijie 2014-06-26 某个Server上在线Producer列表界面
  */
-public class ProducerListActivity extends Activity implements 
+public class ProducerListActivity extends Activity implements
 		OnItemClickListener {
 	private static final boolean BUG = true;
 	private static final String TAG = "ProducerListActivity";
@@ -41,8 +48,14 @@ public class ProducerListActivity extends Activity implements
 	private ListView mListView;
 	private MyAdapter mAdapter;
 	private List<ProducerBeans> mProducerList = new ArrayList<ProducerBeans>();
+	private ServerBeans mServerBeans;
 	private ActionBar mActionBar;
 
+	private Handler myHandler = new Handler(){
+		public void handleMessage(android.os.Message msg) {
+			
+		};
+	};
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -55,13 +68,13 @@ public class ProducerListActivity extends Activity implements
 
 	private void initView() {
 		mActionBar = getActionBar();
-        mActionBar.setDisplayHomeAsUpEnabled(true);
-        mActionBar.setHomeButtonEnabled(true);
-        mActionBar.setBackgroundDrawable(getResources().getDrawable(
-                R.color.actionbar_background));
-        mActionBar.setIcon(R.drawable.ic_launcher);
-        mActionBar.setTitle("Producer");
-        
+		mActionBar.setDisplayHomeAsUpEnabled(true);
+		mActionBar.setHomeButtonEnabled(true);
+		mActionBar.setBackgroundDrawable(getResources().getDrawable(
+				R.color.actionbar_background));
+		mActionBar.setIcon(R.drawable.ic_launcher);
+		mActionBar.setTitle("Producer");
+
 		mListView = (ListView) findViewById(R.id.producer_list);
 	}
 
@@ -72,6 +85,51 @@ public class ProducerListActivity extends Activity implements
 	private void initData() {
 		mAdapter = new MyAdapter(mContext);
 		mListView.setAdapter(mAdapter);
+		mServerBeans = (ServerBeans) getIntent().getSerializableExtra(
+				"serverBeans");
+		requestLinkServer(mServerBeans);
+	}
+
+	// 链接服务器，并接受服务器返回的Producer列表
+	private void requestLinkServer(ServerBeans serverBeans) {
+		ObjectInputStream is = null;
+		ObjectOutputStream os = null;
+		try {
+			Socket socket = new Socket(serverBeans.getIp(),
+					serverBeans.getPort());
+			is = new ObjectInputStream(socket.getInputStream()); // 从socket流中接收数据
+
+			os = new ObjectOutputStream(socket.getOutputStream());
+			PacketBean packetBean = new PacketBean();
+			packetBean.setPacketType(PacketBean.PRODUCER_LIST);
+			os.writeObject(packetBean);
+			os.flush();
+
+			packetBean = (PacketBean) is.readObject();
+			if (packetBean != null) {
+				if (packetBean.getPacketType() == PacketBean.PRODUCER_LIST) {
+					mProducerList = (List<ProducerBeans>) packetBean.getData();
+					myHandler.sendEmptyMessage(0);
+				}
+			}
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (is != null) {
+					is.close();
+				}
+				if (os != null) {
+					os.close();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 
 	/**
@@ -80,17 +138,17 @@ public class ProducerListActivity extends Activity implements
 	@Override
 	public void onItemClick(AdapterView<?> arg0, View view, int position,
 			long id) {
-		
+
 	}
-	
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if(item.getItemId() == android.R.id.home){
-            finish();
-        }
-        return super.onOptionsItemSelected(item);
-    }
-    
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		if (item.getItemId() == android.R.id.home) {
+			finish();
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
 	class MyAdapter extends BaseAdapter {
 		LayoutInflater mLayoutInflater;
 		Context mContext;
